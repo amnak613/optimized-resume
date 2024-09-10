c9eddc2c0a100eef6d0d5264ec23fb1d64a129d2
amnak613 <amnak1284@gmail.com>
2024-09-09 21:51:33 -0700

    added bs4 html parser and cleaned up code

--- optimized-resume-builder.py
+++ optimized-resume-builder.py
@@ -1,6 +1,10 @@
 import argparse
 import subprocess
 import time
+import bs4
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import html5lib
 
 # creates optimized resuem bullets within a pdf document using keywords
 # from the job posting to speed up the time to make a resume for each job 
@@ -10,6 +14,21 @@ def optimized_resume_builder():
     #runs bash script
     run_bash_script()
     args = get_command_line_args()
+
+    """ html = urlopen(args.url) 
+    soup = BeautifulSoup(html)
+    print(soup, "html doc") """
+    
+    with urlopen(args.url) as f:
+        soup = BeautifulSoup(f)
+        print(soup)
+    """ with urlopen(args.url) as f:
+        document = html5lib.parse(f, transport_encoding=f.info().get_content_charset())
+       
+        soup.find("element")
+        print(document) """
+
+    
 
     print(args.filename, args.url)
 # uses subprocesses to run bash script     
@@ -27,6 +46,7 @@ def get_command_line_args():
                     help='file for the cli resume')
     parser.add_argument('url', 
                     help='url for the jobsite description')
+    
 
 
     args = parser.parse_args()
